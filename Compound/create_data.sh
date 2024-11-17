ac7f0f6a51d953a27c179a86972fdb098fdcb8fa
ZhangYipeng01 <161797682+ZhangYipeng01@users.noreply.github.com>
2024-11-17 21:45:22 +0800

    Update create_data.sh

--- Compound/create_data.sh
+++ Compound/create_data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-nohup python get_rhomboid_fromSMILES.py --dataset COX2&
+nohup python get_rhomboid.py --dataset COX2&
 wait
 nohup python Dataset_RT_pooling.py --dataset COX2&
 wait
 
-nohup python get_rhomboid_fromSMILES.py --dataset BZR&
+nohup python get_rhomboid.py --dataset BZR&
 wait
 nohup python Dataset_RT_pooling.py --dataset BZR&
 wait
